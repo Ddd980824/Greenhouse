ecb2ea1f927651211976a28233b4b26ee474f42f
dingzijun <719391061@qq.com>
2022-03-23 11:45:38 +0800

    圆面积计算.py

---
+++ 圆面积计算.py
@@ -0,0 +1,15 @@
+radius = 25
+area = 3.1415 * radius *radius
+print(area)
+print("{:.2f}".format(area))
+
+radius = 5
+area = 3.1415 * radius *radius
+print(area)
+print("{:.2f}".format(area))
+
+a = 3.1546
+#字符串格式化输出保留两位小数
+print("{:.2f}".format(a))
+print("{:.2f}".format(0))
+print("{:.0f}".format(a))
